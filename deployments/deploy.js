f7069a696f1df3457aa65b04434301276c78c175
David Alenoghena <davidalenoghena@gmail.com>
2024-08-24 17:57:08 +0100

    updates

--- deployments/deploy.js
+++ deployments/deploy.js
@@ -1,1 +1,18 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
 
+    console.log(
+        "Deploying contracts with the account:",
+        deployer.address
+    );
+
+    const CrowdFunding = await ethers.getContractFactory("Crowdfunding");
+    const crowdfund = await CrowdFunding.deploy();
+    console.log("Contract Deployed to Address:", crowdfund.address);
+}
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,16 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config()
+require("@nomiclabs/hardhat-ethers");
+
+const { EndPointURL, PrivateKEY } = process.env;
+
 module.exports = {
-  solidity: "0.8.24",
-};
+    solidity: "0.8.0",
+    defaultNetwork: "sepolia",
+    networks: {
+        hardhat: {},
+        sepolia: {
+            url: EndPointURL,
+            accounts: [`0x${PrivateKEY}`]
+        }
+    },
+}
